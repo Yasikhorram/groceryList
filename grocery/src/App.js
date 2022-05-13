4e45461c42d4cbeba4eeadf8cb5b7a0129b65b26
Yasikhorram <khorramyasaman@gmail.com>
2022-05-12 23:34:40 -0500

    add new function to add the item

--- grocery/src/App.js
+++ grocery/src/App.js
@@ -26,6 +26,14 @@ function App() {
   ]);
   const [newItem, setNewItem] = useState("");
 
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const myNewItem = { id, checked: false, item };
+    const listItems = [...items, myNewItem];
+    setItem(listItems);
+    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+  };
+
   const handleNameChange = () => {
     let names = ["Yasi", "Yasaman", "YasamanJoon"];
     let random = Math.floor(Math.random() * names.length);
